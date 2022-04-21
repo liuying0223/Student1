f461965d620f5c3ec19243c0f1646270afd07f19
hp <hp@DESKTOP-U9SML5Q>
2022-04-21 16:39:07 +0800

    添加项目文件。

---
+++ Student1/Data/Student1Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Student1.Models;
+
+namespace Student1.Data
+{
+    public class Student1Context : DbContext
+    {
+        public Student1Context (DbContextOptions<Student1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student1.Models.Student> Student { get; set; }
+    }
+}
---
+++ Student1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Nameuse { get; set; }
+        public string? Gender { get; set; }
+        public int Age { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? Brith { get; set; }
+        public string? phon { get; set; }
+        public float? Tem { get; set; }
+        public DateTime? Datetime { get; set; }
+    }
+}
